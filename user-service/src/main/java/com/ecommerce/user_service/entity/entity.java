1277ea3f8a918b9cd0808cb1f94b51040b8d41c9
abhi.tomar <abhishek.tomar1@mphasis.com>
2026-03-08 10:44:17 +0530

    User Entity and roles created

---
+++ user-service/src/main/java/com/ecommerce/user_service/entity/entity.java
@@ -0,0 +1,23 @@
+package com.ecommerce.user_service.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+}
+
