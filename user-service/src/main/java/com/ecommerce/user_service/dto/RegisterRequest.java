e6ff1c86110719c8785e972c2c96462ba688854a
abhi.tomar <abhishek.tomar1@mphasis.com>
2026-03-08 11:19:06 +0530

    DTO classes imple

--- user-service/src/main/java/com/ecommerce/user_service/dto/RegisterRequest.java
+++ user-service/src/main/java/com/ecommerce/user_service/dto/RegisterRequest.java
@@ -1,4 +1,44 @@
+
 package com.ecommerce.user_service.dto;
 
+import com.ecommerce.user_service.entity.Role;
+
 public class RegisterRequest {
+
+    private String username;
+    private String email;
+    private String password;
+    private Role role;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
