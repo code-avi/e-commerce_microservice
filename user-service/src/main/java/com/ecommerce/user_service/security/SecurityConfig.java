8e54c50d44ed8bd6821ee4f3e9cd3a62e57e1442
abhi.tomar <abhishek.tomar1@mphasis.com>
2026-03-09 13:29:23 +0530

    Added Bcrypt password encoder

--- user-service/src/main/java/com/ecommerce/user_service/security/SecurityConfig.java
+++ user-service/src/main/java/com/ecommerce/user_service/security/SecurityConfig.java
@@ -1,12 +1,12 @@
 package com.ecommerce.user_service.security;
 
-
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig {
@@ -26,4 +26,10 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
