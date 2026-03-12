62482609c0347be1d24f15b5f093f5edeb436326
abhi.tomar <abhishek.tomar1@mphasis.com>
2026-03-12 14:53:06 +0530

    user service completed with user auth

--- user-service/src/main/java/com/ecommerce/user_service/repository/UserRepository.java
+++ user-service/src/main/java/com/ecommerce/user_service/repository/UserRepository.java
@@ -1,14 +1,14 @@
-package com.ecommerce.user_service.repository;
+    package com.ecommerce.user_service.repository;
 
 
-import com.ecommerce.user_service.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+    import com.ecommerce.user_service.entity.User;
+    import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+    import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+    public interface UserRepository extends JpaRepository<User, Long> {
 
-    // Custom query method to find a user used during login to verify user
-    Optional<User> findByUsername(String username);
+        // Custom query method to find a user used during login to verify user
+        Optional<User> findByUsername(String username);
 
-}
+    }
